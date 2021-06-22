a08eca22f296aa1ace84fd3ce2599fa66270e3c6
Jean-Michel Paris <jean_michel_paris@hotmail.fr>
2021-06-22 16:27:35 +0200

    lesson 46 updated

--- lesson-46/46_multithreading.cpp
+++ lesson-46/46_multithreading.cpp
@@ -44,16 +44,16 @@ class LTexture
 		void free();
 
 		//  Set color modulation
-		void setColor( Uint8 red, Uint8 green, Uint8 blue );
+		void setColor   ( Uint8 red, Uint8 green, Uint8 blue );
 
 		//  Set blending
 		void setBlendMode( SDL_BlendMode blending );
 
 		//  Set alpha modulation
-		void setAlpha( Uint8 alpha );
+		void setAlpha   ( Uint8 alpha );
 		
 		//  Renders texture at given point
-		void    render          (
+		void    render                  (
                     int                 x       ,
                     int                 y       ,
                     SDL_Rect*           clip    =   NULL    ,
